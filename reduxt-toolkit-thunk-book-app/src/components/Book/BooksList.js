4539f8c492b197aed4eb87ac6fc89e042dac8521
RafiqSultan <Rafiqaasultan@gmail.com>
2022-10-18 12:10:38 -0700

    INSERT data form to server

--- reduxt-toolkit-thunk-book-app/src/components/Book/BooksList.js
+++ reduxt-toolkit-thunk-book-app/src/components/Book/BooksList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const BooksList = ({isLoading , data}) => {
-const bookTitle = data.map((item) => (
+const bookTitle = data.length > 0 ? (data.map((item) => (
  <li className='list-group-item d-flex  justify-content-between align-items-center' key={item.id}>
           <div>{item.title}</div>
           <div className='btn-group' role='group'>
@@ -12,10 +12,15 @@ const bookTitle = data.map((item) => (
               Delete
             </button>
           </div>
-        </li> 
+        </li> )))
+        :
+          (<div className='alert alert-secondary' role='alert'>
+          There is no books inserted yet!
+        </div>);
+        
 
 
-));
+
 // const bookTitle = data.map(el => (<li>{el.title}</li>))
   return (
     <div> 
@@ -27,6 +32,10 @@ const bookTitle = data.map((item) => (
           
           </ul>
         }
+        
+          
+        
+       
     </div>
   );
 };
--- reduxt-toolkit-thunk-book-app/src/store/bookSlice.js
+++ reduxt-toolkit-thunk-book-app/src/store/bookSlice.js
@@ -1,6 +1,8 @@
 import { createSlice , createAsyncThunk } from "@reduxjs/toolkit";
 
+// Async for initail
 export const getBooks=createAsyncThunk("books/getBooks",async(_, thunkAPI)=> {
+    const {rejectWithValue}=thunkAPI;
     try{
         // Connect with API
         const result= await fetch("http://localhost:3005/books");
@@ -8,17 +10,37 @@ export const getBooks=createAsyncThunk("books/getBooks",async(_, thunkAPI)=> {
         return data;
 
     } catch(error){
-        console.log(error);
+        return rejectWithValue(error.message);
     }
 
 });
-
+// Async for insert books
+export const insertBook= createAsyncThunk("books/insertBooks",async( dataBook,thunkAPI) => {
+    const {rejectWithValue}=thunkAPI;
+try {
+    const insert= await fetch("http://localhost:3005/books", {
+        method:'POST',
+        body:JSON.stringify(dataBook),
+        Headers:{
+            'Content-type':'application/json; charset=UTF-8',
+        },
+    });
+    const data=await insert.json();
+    return data;
+    
+} catch (error) {
+    return rejectWithValue(error.message);
+}
+});
 const bookSlice= createSlice({
     name:"book",
-    initialState:{books:[], isLoading:false},
+    initialState:{books:[], isLoading:false,error:null},
     extraReducers:{
-        [getBooks.pending]:(state , action)=>{
+
+        // get books
+         [getBooks.pending]:(state , action)=>{
             state.isLoading=true;
+            state.error=null;
             
         },
         [getBooks.fulfilled]:(state , action)=>{
@@ -28,6 +50,26 @@ const bookSlice= createSlice({
         },
         [getBooks.rejected]:(state , action)=>{
             state.isLoading=false;
+            state.error=action.payload;
+            
+        },
+
+        // insert books
+        [insertBook.pending]:(state , action)=>{
+            state.isLoading=true;
+            state.error=null;
+            
+        },
+        [insertBook.fulfilled]:(state , action)=>{
+            state.isLoading=false;
+            // push data into satate reducer books
+            console.log(action.payload);
+            // state.books.push(action.payload);
+        },
+        [insertBook.rejected]:(state , action)=>{
+            state.isLoading=false;
+           
+            state.error=action.payload;
             
         }
     }
